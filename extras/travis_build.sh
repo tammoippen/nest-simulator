ea67c94ddd3853ab3d6c14b5ba7ed28e391e3529
Tammo Ippen <tammo.ippen@posteo.de>
2017-10-27 15:45:35 +0200

    xyz

--- extras/travis_build.sh
+++ extras/travis_build.sh
@@ -237,7 +237,6 @@ echo "+ + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + 
 echo "+               I N S T A L L   P y N E S T                                   +"
 echo "+ + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + +"
 echo "MSGBLD0283: Running sudo pip install ../pynest."
-echo 'env'
 sudo -H pip --no-cache-dir install -v ../pynest
 echo "MSGBLD0286: PyNEST install completed."
 fi
